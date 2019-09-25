64ece7a5a4601fb590e6bea3b7ed5a095eccaf48
Mohammad Javed <ghazanfar9131@gmail.com>
2019-09-25 15:28:46 -0400

    - Fixed Signup Bug.

--- js/signup.js
+++ js/signup.js
@@ -1,16 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-let email = document.getElementById("exampleInputEmail1").value;
-let password = document.getElementById("exampleInputPassword1").value;
+    document.getElementById('signupBtn').addEventListener('click', (event) => {
 
+        let email = document.getElementById("exampleInputEmail1").value;
 
-fetch("http://thesi.generalassemb.ly:8080/signup",
-{
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    method: "POST",
-    body: JSON.stringify({'email': `${email}`, 'password': `${password}`,'username':`${email}`})
-})
-.then(function(res){ console.log(res) })
-.catch(function(res){ console.log(res) })
+        let password = document.getElementById("exampleInputPassword1").value;
+
+        event.preventDefault();
+        console.log(email);
+        console.log(password);
+
+        fetch("http://thesi.generalassemb.ly:8080/signup",
+        {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({'email': `${email}`, 'password': `${password}`,'username':`${email}`})
+        })
+        .then(res=> return res.json() ).then(res=>{console.log(res)})
+        .catch((res)=>{ console.log("Error:"+ res) })
+    })
+});
