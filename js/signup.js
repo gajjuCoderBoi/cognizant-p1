13faac5aeb8c96c225db3ac6210d01082b3a06ea
Mingjing Tang <tmjcsh@gmail.com>
2019-09-26 11:03:21 -0400

    sign up form information submitted

--- js/signup.js
+++ js/signup.js
@@ -1,38 +1,49 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('signupBtn').addEventListener('click', (event) => {
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("signupBtn").addEventListener("click", event => {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let addtionalEmail = document.getElementById("additionalEmail").value;
+    let password = document.getElementById("password").value;
+    let passwordConfirm = document.getElementById("passwordConfirm").value;
+    let username = document.getElementById("userName").value;
+    let mobile = document.getElementById("mobile").value;
+    let address = document.getElementById("address").value;
 
-        let email = document.getElementById("email").value;
-        let addtionalEmail = document.getElementById("addtionalEmail").value;
-        let password = document.getElementById("password").value;
-        let passwordConfirm = document.getElementById("passwordConfirm").value;
-        let username = document.getElementById("userName").value;
-        let mobile = document.getElementById("mobile").value;
-        let address = document.getElementById("address").value;
+    console.log(email);
+    console.log(addtionalEmail);
+    console.log(password);
+    console.log(passwordConfirm);
+    console.log(username);
+    console.log(mobile);
+    console.log(address);
 
-        document.querySelector("#signUp").addEventListener("submit",e=>{
-            e.preventDefault();
+    if (
+      email !== "" &&
+      password !== "" &&
+      passwordConfirm !== "" &&
+      password === passwordConfirm
+    ) {
+      fetch("http://thesi.generalassemb.ly:8080/signup", {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+          email: `${email}`,
+          password: `${password}`,
+          username: `${email}`
         })
-
-        event.preventDefault();
-        console.log(email);
-        console.log(addtionalEmail);
-        console.log(password);
-        console.log(passwordConfirm);
-        console.log(username);
-        console.log(mobile);
-        console.log(address);
-       
-
-
-        // fetch("http://thesi.generalassemb.ly:8080/signup",
-        // {
-        // headers: {
-        //     'Content-Type': 'application/json'
-        // },
-        // method: "POST",
-        // body: JSON.stringify({'email': `${email}`, 'password': `${password}`,'username':`${email}`})
-        // })
-        // .then(res=> res.json() ).then(res=>{console.log()})
-        // .catch((res)=>{ console.log("Error:"+ res) })
-    })
+      })
+        .then(res => res.json())
+        .then(res => {
+          console.log(res);
+        })
+        .catch(res => {
+          console.log("Error:" + res);
+        });
+    }
+    else{
+        alert("please enter again!");
+    }
+  });
 });
