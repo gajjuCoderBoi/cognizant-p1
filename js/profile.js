8dfd66fb29e9f5005a0f0f2411bd9aa9692eb463
Mingjing Tang <tmjcsh@gmail.com>
2019-09-27 10:16:15 -0400

    post profile first test 500

--- js/profile.js
+++ js/profile.js
@@ -2,20 +2,54 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("updateBtn").addEventListener("click", event => {
     event.preventDefault();
 
-    let email = document.getElementById("email").value;
+    // let email = document.getElementById("email").value;
     let addtionalEmail = document.getElementById("additionalEmail").value;
-    let password = document.getElementById("password").value;
-    let passwordConfirm = document.getElementById("passwordConfirm").value;
-    let username = document.getElementById("userName").value;
+    // let password = document.getElementById("password").value;
+    // let passwordConfirm = document.getElementById("passwordConfirm").value;
+    // let username = document.getElementById("userName").value;
     let mobile = document.getElementById("mobile").value;
     let address = document.getElementById("address").value;
 
-    console.log(email);
+    // console.log(email);
     console.log(addtionalEmail);
-    console.log(password);
-    console.log(passwordConfirm);
-    console.log(username);
+    // console.log(password);
+    // console.log(passwordConfirm);
+    // console.log(username);
     console.log(mobile);
     console.log(address);
+
+    let user = JSON.parse(localStorage.getItem('foodieUser'));
+
+    fetch(`http://thesi.generalassemb.ly:8080/profile`,{
+        method: 'POST',
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${user.token}`
+            // 'Authorization': `Bearer ${localStorage.token}`
+        },
+        body: JSON.stringify({
+            // "additionalEmail" : addtionalEmail,
+            "mobile" : mobile,
+            // "Address": address,
+        })
+    }).then(r=>{
+        if(r.status === 200){
+            // window.location.reload();
+            let inform = document.createElement("p");
+            let textInform = document.createTextNode("Your profile has been updated!");
+            inform.appendChild(textInform);
+            
+            document.getElementById("update").appendChild(inform);
+        }
+    })
+
+
+
   });
+
+
+
+
+
+
 });
