54ccdf005a10740eafc8bac9eaf0930461d9d30a
Mohammad Javed <ghazanfar9131@gmail.com>
2019-09-27 11:07:54 -0400

    - Cleaning up some code.

--- js/profile.js
+++ js/profile.js
@@ -1,37 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
-  getProfile();
-    async function getProfile() {
-      let user = JSON.parse(localStorage.getItem("foodieUser"));
-      if (user) {
-          let token = user.token;
-          let r = await fetch('http://thesi.generalassemb.ly:8080/profile', {
-              method: "GET",
-              headers: {
-                  'Content-Type': 'application/json',
-                  'Authorization': `Bearer ${token}`
-              },
-          })
-          .then(res=>res.json())
-          .then(res=>{
-            document.getElementById("additionalEmail").value = res.additionalEmail;
-            document.getElementById("userName").value = res.user.username;
-            document.getElementById("mobile").value = res.mobile;
-            document.getElementById("address").value = res.address;
-            console.log(res);
-          })
-          .catch(e => console.log(e))
-      }
-  }
+    let user = JSON.parse(localStorage.getItem('foodieUser'));
+    (
+        async function getProfile() {
+            if (user) {
+                let token = user.token;
+                console.log(token);
+                let r = await fetch('http://thesi.generalassemb.ly:8080/profile', {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    },
+                })
+                    .then(res=>res.json())
+                    .then(res=>{
+                        document.getElementById("additionalEmail").value = res.additionalEmail;
+                        document.getElementById("userName").value = res.user.username;
+                        document.getElementById("mobile").value = res.mobile;
+                        document.getElementById("address").value = res.address;
+                        console.log(res);
+                    })
+                    .catch(e => console.log(e))
+            }
+        }
+    )();
+
 
   document.getElementById('homeBtn').addEventListener('click', e=>{
       e.preventDefault();
-
       window.location.href = 'index.html'
   });
 
   document.getElementById("updateBtn").addEventListener("click", event => {
     event.preventDefault();
-
 
     let addtionalEmail = document.getElementById("additionalEmail").value;
     let username = document.getElementById("userName").value;
@@ -43,31 +44,30 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(mobile);
     console.log(address);
 
-    let user = JSON.parse(localStorage.getItem('foodieUser'));
+    updateProfile();
 
+  function updateProfile() {
+      fetch(`http://thesi.generalassemb.ly:8080/profile`,{
+          method: 'POST',
+          headers:{
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${user.token}`
+          },
+          body: JSON.stringify({
+              "additionalEmail" : addtionalEmail,
+              "mobile" : mobile,
+              "address": address,
+          })
+      }).then(r=>{
+          if(r.status === 200){
+              // window.location.reload();
+              let inform = document.createElement("p");
+              let textInform = document.createTextNode("Your profile has been updated!");
+              inform.appendChild(textInform);
 
-    fetch(`http://thesi.generalassemb.ly:8080/profile`,{
-        method: 'POST',
-        headers:{
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${user.token}`
-            // 'Authorization': `Bearer ${localStorage.token}`
-        },
-        body: JSON.stringify({
-            "additionalEmail" : addtionalEmail,
-            "mobile" : mobile,
-            "address": address,
-        })
-    }).then(r=>{
-        if(r.status === 200){
-            // window.location.reload();
-            let inform = document.createElement("p");
-            let textInform = document.createTextNode("Your profile has been updated!");
-            inform.appendChild(textInform);
-
-            document.getElementById("update").appendChild(inform);
-        }
-    })
-  });
-
-});
+              document.getElementById("update").appendChild(inform);
+          }
+      })
+  }
+})
+})
